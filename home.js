b5a0e78f14317e12c2f29dd9cfa225c10b2ba036
Yatharth Agarwal <yatharthagrawal@gmail.com>
2018-11-24 22:33:52 +0530

    second commit

--- home.js
+++ home.js
@@ -2,14 +2,27 @@ var express=require('express');
 var ejs=require('ejs');
 var path = require('path');
 var mysql=require('mysql');
-var popup=require('js-popup');
+var validator = require('validator');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app=express();
-
+var x;
+var p;
 
 app.set('view engine', 'ejs');
 var publicDir = require('path').join(__dirname,'/image');
 app.use(express.static(publicDir));
+app.use(cookieParser());
+// app.use(session({
+//     secret: 'abcd',
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: { secure: true }
+//   }));
+
+app.use(session({secret: 'ssshhhhh'}));
+
 app.use(bodyParser.urlencoded({extended: true}));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -53,29 +66,49 @@ app.post('/login1',urlencodedParser,function(req,res){
 
     var email=req.body.email;
     var password=req.body.password;
- 
     let q = "SELECT * FROM userdetail WHERE Username = ? and Password=?";
     let data = [email,password]
+    let flag = 0
     con.query(q, data, (err,result) => {
         if (err) 
         throw err;
 
         else if(result.length >0)
         {
-			if(result[0].Password == password)
-            res.render(path.join('/home/yatharth/Desktop/dbms_project/views'+'/dashboard.ejs'));
-        }
-        
-       else
-        {
-           res.send(500,'Incorrect Information');
+            if(result[0].Password == password)
+            {
+                //p=result[0].User_Id;
+                req.session.user=result[0].User_Id;
+                x=req.session.user;
+                flag = 1
+                console.log("this is session1 => " + req.session.user)
+                //console.log(req.cookies);
+               // console.log('******************');
+                //console.log(req.session);
+                //res.render('profile',{ "user" : x});
+            }
         }
     });
-        console.log("checked");
+
+    setTimeout(() => {
+        if(flag){
+            console.log("checked");
+            console.log("this is session2 => " + req.session.user)
+            res.render('dashboard');
+        }
+        else{
+            res.send(500,'Incorrect Information');
+        }
+    }, 100)
         
 });
 
-
+app.get('/out',function(req,res){
+    req.session.destroy(function(err) {
+        if(err) throw err;
+        res.render('home1');
+      })
+});
 
 app.post('/register1',urlencodedParser,function(req,res){
 
@@ -83,15 +116,30 @@ app.post('/register1',urlencodedParser,function(req,res){
     var mobile=req.body.number;
     var username=req.body.email;
     var password=req.body.password;
-
-    let q = "INSERT INTO userdetail VALUES(null, ?, ?, ?, ?,'');"
-    let data = [names ,mobile, username, password]
-    con.query(q, data, (err,result) => {
+    if((mobile>0)&&(mobile.length==10))
+    {
+       let p = "INSERT INTO userdetail VALUES(null, ?, ?, ?, ?,'');"
+       let data = [names ,mobile, username, password]
+         con.query(p, data, (err,result) => {
         if (err) throw err;
-
+        let q="SELECT User_Id from userdetail where Username=?";
+        let data1 = [username];
+        con.query(q,data1,(error,results) => {
+            if(error) throw error;
+        let r ="INSERT INTO Wallet VALUES(?,0);"
+        let data2 = [results[0].User_Id]
+           con.query(r,data2,(error,results) => {
+            if(error) throw error;
+         });
+        });
         console.log("Account created");
         res.render(path.join('/home/yatharth/Desktop/dbms_project/views'+'/register.ejs'));
-    });
+      });
+   }
+   else
+   {
+        res.send(500,'incorrect data');
+   }
 });
    
 app.post('/givepassword',urlencodedParser,function(req,res){
@@ -110,7 +158,27 @@ app.post('/givepassword',urlencodedParser,function(req,res){
 });
 
 app.get('/profile',function(req,res){
-    res.render(path.join('/home/yatharth/Desktop/dbms_project/views'+'/profile.ejs'));
+       // let h=x;
+       //var html=new ejs({url: 'profile.ejs'}).render(x); 
+       //document.getElementById("container").innerHTML=html;
+       console.log("this is session1 => " + req.session.user)
+       let qs = "SELECT * FROM userdetail WHERE User_Id=" + req.session.user
+       let row = []  
+       con.query(qs, (err, result) => {
+            if(err) console.error(err)
+
+            row = result
+        })
+
+       setTimeout(() => {
+           if(row.length > 0){
+            res.render('profile',{ "user" : row[0]});
+           }
+           else{
+               res.redirect('/')
+           }
+
+       }, 100)
     
 });
 
@@ -125,8 +193,66 @@ app.get('/bank',function(req,res){
 });
 
 app.get('/pay',function(req,res){
-    res.render(path.join('/home/yatharth/Desktop/dbms_project/views'+'/pay.ejs'));
+    let user = req.session.user
+    let q="SELECT Amount_in_wallet FROM Wallet WHERE Wallet_id=" + user;
+    let row = []
+    con.query(q,(err,result) =>{
+        if(err) throw err;
+        row = result
+    });
+
+    setTimeout(() => {
+        if(row.length > 0){
+            res.render('pay',{ "balance" : row[0]})
+        }
+        else{
+            res.render('dashboard')
+        }
+    }, 100);    
     
+});
+
+app.post('/payment',function(req,res){
+    var t;
+ let user=req.session.user;
+ var mob=req.body.number;
+ var money=req.body.money;
+ let q="select * from userdetail where mobile=?";
+ let data=[mob];
+ con.query(q,data,(error,result)=>{
+    if(error) throw error;
+     t=result[0].User_Id;
+});
+    con.beginTransaction(function(err) {
+    if (err) { throw err; }
+    con.query("UPDATE Wallet SET Amount_in_wallet=Amount_in_wallet+"+money+" WHERE Wallet_id="+t+";", function (error, results, fields) {
+      if (error) {
+        return con.rollback(function() {
+          throw error;
+        });
+      }
+      //else
+      //res.render('pay',{balance : results[0]});
+        con.query("UPDATE Wallet SET Amount_in_wallet=Amount_in_wallet-"+money+" WHERE Wallet_id="+user +";", function (error, results1, fields) {
+        if (error) {
+          return con.rollback(function() {
+            throw error;
+          });
+        }
+        con.commit(function(err) {
+          if (err) {
+            return con.rollback(function() {
+              throw err;
+            });
+          }
+          console.log('success!');
+        });
+        res.render('pay',{balance : results1[0]});
+    });
+    });
+  });
+
+   res.render('dashboard');
 });
 
 app.get('/das',function(req,res){
@@ -134,7 +260,10 @@ app.get('/das',function(req,res){
     
 });
 
+app.get('/addit',function(req,res){
+     
+});
+
 app.listen(8000,function(){
     console.log('listening to server 8000 ');
  });
-
